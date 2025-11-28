122f5d81f7a99edbb3af0028b29ca6a02931766c
sha0256 <97352933+sha0256@users.noreply.github.com>
2025-11-28 12:42:36 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,7 @@
-const CACHE_NAME = "pwa-demo-cache-v1";
-const FILES_TO_CACHE = [
-    "index.html",
-    "app.js",
-    "manifest.json",
-    "service-worker.js"
-];
-
-// התקנה — הכנסת הקבצים לזיכרון Cache
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-    console.log("Service Worker: installed");
-    self.skipWaiting();
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
 
-// הפעלה
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys.map(key => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            )
-        )
-    );
-    console.log("Service Worker: activated");
-});
-
-// בקשות — אם אין אינטרנט, נשתמש ב-Cache
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            // אם יש בקובץ במטמון – תחזיר אותו
-            if (response) {
-                return response;
-            }
-            // אם אין במטמון – נביא מהאינטרנט
-            return fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', (e) => {
+  // ניתן להוסיף כאן קאש לאופליין
 });
